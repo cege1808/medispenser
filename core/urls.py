c9d12c9dd2888522d76e55b49c1906d5faacbd9e
aeronah <aeronah@hotmail.com>
2018-01-28 00:47:18 -0800

    Added user registration page, connected to django admin database. Access from /register.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import UserFormView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='hello'),
-    path('signup', views.signup, name = 'signup')
+    path('signup/', views.signup, name = 'signup'),
+    path('register/', UserFormView.as_view(), name = 'register')
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 # Create your views here.
 def hello(request):
-  return render(request, "hello.html", {"name": "alex"})
+	return render(request, "hello.html", {"name": "alex"})
 
 def index(request):
   return render(request, "index.html")
 
 def signup(request):
-  return render(request, "signup.html")
+	# template = loader.get_template('signup.html')
+	return render(request, 'signup.html', {})
+
+class UserFormView(View):
+	form_class = UserForm
+	template_name = 'registration_form.html'
+
+	# GET and POST built in functions
+
+	# Display blank form
+	def get(self, request):
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form': form})
+
+	# Process form data
+	def post(self, request):
+		form = self.form_class(request.POST)
+
+		if form.is_valid():
+
+			user = form.save(commit=False)
+
+			#cleaned (normalized) data
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user.set_password(password)
+			user.save()
+
+			# Returns user objects if credentials are correct
+			user = authenticate(username=username, password=password)
+
+			if user is not None:
+
+				if user.is_active:
+
+					login(request, user)
+					return redirect('index')
+
+		return render(request, self.template_name, {'form': form})
+
