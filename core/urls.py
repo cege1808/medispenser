a00db099eaf8cbb0aa9e36a4d179a0e46c34161d
cege1808 <gunawan.clarissa@hotmail.com>
2018-02-24 19:27:50 -0800

    change views function to views.function

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
-from .views import login_view, logout_view, edit_profile, create_user
+# from .views import login_view, logout_view, edit_profile, create_user
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', create_user, name = 'register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('profile/edit/', edit_profile, name='profile/edit')
+    path('register/', views.create_user, name = 'register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/edit/', views.edit_profile, name='profile/edit'),
+    path('demo/', views.demo, name='demo')
 ]
