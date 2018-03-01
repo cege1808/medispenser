1fd16828d340d1be1249fd5c5be7a5a016177071
cege1808 <gunawan.clarissa@hotmail.com>
2018-03-01 03:08:33 -0800

    test remove channel layer

--- medispenser/settings.py
+++ medispenser/settings.py
@@ -83,19 +83,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'medispenser.wsgi.application'
 
 ##### Channels-specific settings
-redis_host = os.environ.get('REDIS_HOST', 'localhost')
+# redis_host = os.environ.get('REDIS_HOST', 'localhost')
 
 # Channel layer definitions
 # http://channels.readthedocs.io/en/latest/topics/channel_layers.html
-CHANNEL_LAYERS = {
-    "default": {
-        # This example app uses the Redis channel layer implementation channels_redis
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [(redis_host, 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     "default": {
+#         # This example app uses the Redis channel layer implementation channels_redis
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [(redis_host, 6379)],
+#         },
+#     },
+# }
 
 # ASGI_APPLICATION should be set to your outermost router
 ASGI_APPLICATION = "medispenser.routing.application"
