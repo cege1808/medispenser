76b320627b64367d6c9e019618f9f72e62721797
cege1808 <gunawan.clarissa@hotmail.com>
2018-03-03 01:11:58 -0800

    clean redis_url

--- medispenser/settings.py
+++ medispenser/settings.py
@@ -162,7 +162,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CELERY_RESULT_BACKEND = config('CELERY_BROKER_URL', default='redis://')
-CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = config('REDIS_URL', default='redis://')
+CELERY_BROKER_URL = config('REDIS_URL', default='redis://localhost:6379/0')
 CELERY_TASK_SERIALIZER='json'
 
